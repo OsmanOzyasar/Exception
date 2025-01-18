ff67e3fa36d1051729952cfb8e4e1eda0a983dd0
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-18 19:47:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+
+            bool isTrue = false;
+            int userInput = 0;
+            while (!isTrue)
+            {
+                try
+                {
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    isTrue = true;
+
+                }
+                catch (System.Exception)
+                {
+
+                    Console.WriteLine("Geçerli bir sayı değeri giriniz.");
+                }
+            }
+
+            int result = userInput * userInput;
+
+            Console.WriteLine("Sayının karesi -> " + result);
+			
+        }
+    }
+}
